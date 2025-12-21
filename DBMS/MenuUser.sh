fb06b9a77287964a9d7455220862e42102cf2818
Ziad Osama <120169854+ziadosama74@users.noreply.github.com>
2025-12-21 18:11:51 +0200

    Add files via upload

--- DBMS/MenuUser.sh
+++ DBMS/MenuUser.sh
@@ -46,10 +46,10 @@ function CreateTable {
 		echo " -------------------------------------------"
 		sleep 2
 		clear
-		# Open the Create table Scipt file
+		# Open Create table Scipt file
 		#---------------------------------
-		#./CreatTable.sh "$DBPath/$TableName" "$TableName"
-		#                Table Path File      Name in UI
+		./CreatTable.sh "$DBPath/$TableName" "$TableName" "$DBName"
+	
 	fi
 }
 
@@ -105,18 +105,142 @@ function DropTable {
 #----------------------------------------------------------------
 #               Inert Data into Table Function
 #----------------------------------------------------------------
+function InsertDataAtTable {
+	if ! ListAllTable; then
+                echo " No Tables to Insert Data Into !!"
+                sleep 2
+                return 1
+        fi
+	echo " ----------------------------------- "
+        read -p " Enter The Table Name to Insert Data inside it :   " TableName
+        [ -z "$TableName" ] && echo " Table name cannot be empty :(" && sleep 2 && clear && return 1 # Can not accept the empty file
+	if TableIsFound "$TableName"; then
+               
+                echo " Now You Are Accessing $TableName Table  :)"
+                sleep 2
+		clear
+		# Open Insert Data into table Scipt file
+                #---------------------------------------------
+                ./InsertDataInTable.sh "$DBPath/$TableName" "$TableName" "$DBName"
+
+        else
+                echo " $TableName Is Not Existed "
+        fi
+        sleep 2
+        clear
+
+}
 #----------------------------------------------------------------
 #              Update Data in a Table Function
 #----------------------------------------------------------------
+function UpdateDataAtTable {
+        if ! ListAllTable; then
+                echo " No Tables to Update Data Into !!"
+                sleep 2
+                return 1
+        fi
+        echo " ----------------------------------- "
+        read -p " Enter The Table Name to Update Data inside it :   " TableName
+        [ -z "$TableName" ] && echo " Table name cannot be empty :(" && sleep 2 && clear && return 1 # Can not accept the empty file
+        if TableIsFound "$TableName"; then
+
+                echo " Now You Are Accessing $TableName Table  :)"
+                sleep 2
+                clear
+                # Open Update Data into table Scipt file
+                #---------------------------------------------
+                ./UpdateDataInTable.sh "$DBPath/$TableName" "$TableName" "$DBName"
+
+        else
+                echo " $TableName Is Not Existed "
+        fi
+        sleep 2
+        clear
+}
+
 #----------------------------------------------------------------
 #               Select From Table Function
 #----------------------------------------------------------------
+function SelectFromTable {
+        if ! ListAllTable; then
+                echo " No Tables to Retrive Data from it !!"
+                sleep 2
+                return 1
+        fi
+        echo " ----------------------------------- "
+        read -p " Enter The Table Name to Retrive the Data from it :   " TableName
+        [ -z "$TableName" ] && echo " Table name cannot be empty :(" && sleep 2 && clear && return 1 # Can not accept the empty file
+        if TableIsFound "$TableName"; then
+
+                echo " Now You Are Accessing $TableName Table  :)"
+                sleep 2
+                clear
+                # Open Select Data from  table Scipt file
+                #---------------------------------------------
+                ./SelectDataFromTable.sh "$DBPath/$TableName" "$TableName" "$DBName"
+
+        else
+                echo " $TableName Is Not Existed "
+        fi
+        sleep 2
+        clear
+}
+
 #----------------------------------------------------------------
 #                Delete From Table Function
 #----------------------------------------------------------------
+function DeleteFromTable {
+        if ! ListAllTable; then
+                echo " No Tables to Delete Data from it !!"
+                sleep 2
+                return 1
+        fi
+        echo " ----------------------------------- "
+        read -p " Enter The Table Name to Delete Data from it :   " TableName
+        [ -z "$TableName" ] && echo " Table name cannot be empty :(" && sleep 2 && clear && return 1 # Can not accept the empty file
+        if TableIsFound "$TableName"; then
+
+                echo " Now You Are Accessing $TableName Table  :)"
+                sleep 2
+                clear
+                # Open Delete Data from table Scipt file
+                #---------------------------------------------
+                ./DeleteDataFromTable.sh "$DBPath/$TableName" "$TableName" "$DBName"
+
+        else
+                echo " $TableName Is Not Existed "
+        fi
+        sleep 2
+        clear
+}
+
 #----------------------------------------------------------------
 #              Alter Structure Table Function
 #----------------------------------------------------------------
+function AlterTable {
+        if ! ListAllTable; then
+                echo " No Tables to Modify the Structure of it !!"
+                sleep 2
+                return 1
+        fi
+        echo " ----------------------------------- "
+        read -p " Enter The Table Name to Modify it's Structure :   " TableName
+        [ -z "$TableName" ] && echo " Table name cannot be empty :(" && sleep 2 && clear && return 1 # Can not accept the empty file
+        if TableIsFound "$TableName"; then
+
+                echo " Now You Are Accessing $TableName Table  :)"
+                sleep 2
+                clear
+                # Open Alter table Scipt file
+                #---------------------------------------------
+                ./AlterStructureTable.sh "$DBPath/$TableName" "$TableName" "$DBName"
+
+        else
+                echo " $TableName Is Not Existed "
+        fi
+        sleep 2
+        clear
+}
 
 #================================================================
 #                     The User Menu
@@ -144,6 +268,21 @@ do
 			;;
 		3) clear
 			DropTable
+			;;
+		4) clear
+			InsertDataAtTable
+			;;
+		5) clear
+			UpdateDataAtTable
+			;;
+		6) clear
+			SelectFromTable
+			;;
+		7) clear
+			DeleteFromTable
+			;;
+		8) clear
+			AlterTable
 			;;
 		9) clear
 		       	exit 1
