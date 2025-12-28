841b749b4c675e3cefac4aa2adefa294198fea28
Youssef-Amgad <youssefamgad93@gmail.com>
2025-12-28 18:49:48 +0200

    delate data from table updated

--- DBMS/DeleteDataFromTable.sh
+++ DBMS/DeleteDataFromTable.sh
@@ -1,131 +1,125 @@
-
 #!/bin/bash
+shopt -s extglob
+clear
 
 TablePath="$1"
 TableName="$2"
 DBName="$3"
 
+#---------------------------------------------------------------
+#                    Display All Rows Function
+#---------------------------------------------------------------
+function DisplayAll {
+    echo "======================================"
+    echo "       $TableName Table Data          "
+    echo "======================================"
+    cat "$TablePath" | column -t -s ":"
+    read -p "Press Enter to continue..."
+}
 
 #---------------------------------------------------------------
-#                Delete From Table Function
+#                    Delete Rows Function
 #---------------------------------------------------------------
-function DeleteFromTable {
-
-    while true
-    do
+function DeleteRows {
+    while true; do
         clear
-        echo "==================== ( $DBName DB - $TableName Table ) ===================="
-        echo "1) Display Table"
-        echo "2) Delete All Rows"
-        echo "3) Delete Rows By Column Value"
-        echo "4) Back"
-        echo "========================================================================="
-        read -p "Choose Option : " Choice
+        echo "=========================== ( $DBName DB - $TableName Table ) ====="
+        echo "1) Delete All Rows"
+        echo "2) Delete Rows By Column Values"
+        echo "3) Back"
+        echo "====================================================================="
+        read -p "Enter Option Number: " ChoiceDelete
 
-        case $Choice in
+        case $ChoiceDelete in
+            1)
+                clear
+                Header=$(head -n 1 "$TablePath")
+                echo "$Header" > "$TablePath"
+                echo "All rows deleted successfully."
+                sleep 2
+                ;;
+            2)
+                clear
+                Header=$(head -n 1 "$TablePath")
 
-        #-------------------------------------------------------
-        # Display table
-        #-------------------------------------------------------
-        1)
-            clear
-            cat "$TablePath" | column -t -s ":"
-            read -p "Press Enter to continue..."
-            ;;
+                Columns=()
+                for col in $(echo "$Header" | tr ":" "\n"); do
+                    Columns+=("$(echo "$col" | sed 's/(.*)//')")
+                done
 
-        #-------------------------------------------------------
-        # Delete all rows (keep header)
-        #-------------------------------------------------------
-        2)
-            clear
-            Header=$(head -n 1 "$TablePath")
-            echo "$Header" > "$TablePath"
-            echo "All rows deleted successfully "
-            sleep 2
-            ;;
+                echo "Available Columns: ${Columns[*]}"
+                read -p "Enter column name to delete by: " DelCol
+                read -p "Enter values separated by space: " -a DelVals
 
-        #-------------------------------------------------------
-        # Delete rows by column value
-        #-------------------------------------------------------
-        3)
-             clear
-         Header=$(head -n 1 "$TablePath")
+                ColNum=0
+                for i in "${!Columns[@]}"; do
+                    if [[ "${Columns[$i]}" == "$DelCol" ]]; then
+                        ColNum=$((i+1))
+                        break
+                    fi
+                done
 
-    # Get column names
-    Columns=()
-    for col in $(echo "$Header" | tr ":" "\n"); do
-        Columns+=("$(echo "$col" | sed 's/(.*)//')")
-    done
+                if [[ $ColNum -eq 0 ]]; then
+                    echo "Column '$DelCol' not found!"
+                    sleep 2
+                    continue
+                fi
 
-    echo "Available Columns: ${Columns[*]}"
-    read -p "Enter column name: " ColName
-    read -p "Enter values to delete (space separated): " -a Values
+                tmpfile=$(mktemp)
+                echo "$Header" > "$tmpfile"
 
-    # Find column number
-    ColNum=0
-    for i in "${!Columns[@]}"; do
-        if [[ "${Columns[$i]}" == "$ColName" ]]; then
-            ColNum=$((i+1))
-            break
-        fi
-    done
+                awk -F":" -v col="$ColNum" -v vals="${DelVals[*]}" '
+                BEGIN { split(vals, arr, " ") }
+                NR>1 {
+                    for (i in arr) {
+                        if ($col == arr[i]) next
+                    }
+                    print
+                }' "$TablePath" >> "$tmpfile"
 
-    if [[ $ColNum -eq 0 ]]; then
-        echo "Column not found "
-        sleep 2
-        continue
-    fi
-
-    TempFile=$(mktemp)
-    echo "$Header" > "$TempFile"
-
-    Found=0
-
-    awk -F":" -v col="$ColNum" -v vals="${Values[*]}" '
-    BEGIN {
-        split(vals, arr, " ")
-    }
-    NR>1 {
-        for (i in arr) {
-            if ($col == arr[i]) {
-                found=1
-                next
-            }
-        }
-        print
-    }
-    END {
-        if (found != 1) exit 10
-    }
-    ' "$TablePath" >> "$TempFile"
-
-    if [[ $? -eq 10 ]]; then
-        rm "$TempFile"
-        echo "Value not found in column "
-        sleep 2
-        continue
-    fi
-
-    mv "$TempFile" "$TablePath"
-    echo "Rows deleted successfully "
-    sleep 2
-    ;;
-        #-------------------------------------------------------
-        # Back
-        #-------------------------------------------------------
-        4)
-            break
-            ;;
-
-        *)
-            echo "Invalid Option "
-            sleep 2
-            ;;
+                mv "$tmpfile" "$TablePath"
+                echo "Rows deleted successfully."
+                sleep 2
+                ;;
+            3)
+                break
+                ;;
+            *)
+                echo "Invalid option!"
+                sleep 2
+                ;;
         esac
     done
 }
 
 #---------------------------------------------------------------
-#                       main === Start Delete Menu
+#                            Main Menu
 #---------------------------------------------------------------
-DeleteFromTable
+while true; do
+    clear
+    echo "=========================== ( $DBName DB - $TableName Table ) ====="
+    echo "1) Display Table"
+    echo "2) Delete Rows"
+    echo "3) Back"
+    echo "====================================================================="
+    read -p "Enter Option Number : " Choice
+
+    case $Choice in
+        1)
+            clear
+            DisplayAll
+            ;;
+        2)
+            DeleteRows
+            ;;
+        3)
+            clear
+            break
+            ;;
+        *)
+            clear
+            echo "Invalid Option !!"
+            sleep 2
+            ;;
+    esac
+done
